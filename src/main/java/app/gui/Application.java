26b7ee04c5a505d5f7776361a8b77c643e131670
John <jsgeneralusage@gmail.com>
2026-05-28 20:01:34 -0400

    Update text to include top grade

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -280,7 +280,7 @@ public class Application {
                 JOptionPane.showMessageDialog(jFrame, ex.getMessage());
             }
         });
-        theCard.add(new JLabel("The course you want to calculate the team average for:"));
+        theCard.add(new JLabel("The course you want to calculate the team average and top grade for:"));
         theCard.add(courseField);
         theCard.add(getAverageButton);
         theCard.add(getTopButton);
